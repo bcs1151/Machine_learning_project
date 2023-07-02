cd899df506c626095ad49500b8d384b0a64c32e1
vikas <yadav4228@gmail.com>
2023-07-02 22:16:16 +0530

    setuplast

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 #declaring variables for setup functions
@@ -11,7 +11,7 @@ REQUIREMENT_FILE_NAME='requirements.txt'
 
 def get_requirements_list()-> List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -19,6 +19,6 @@ setup(
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
