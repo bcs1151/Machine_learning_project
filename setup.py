dce8fa7d7211c0f5b82d24687d068888be286471
vikas <yadav4228@gmail.com>
2023-07-02 21:54:11 +0530

    setup.py'

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+from typing import List
+
+#declaring variables for setup functions
+PROJECT_NAME='housing-predictor'
+VERSION='0.0.1'
+AUTHOR='Vikas Yadav'
+DESCRIPTION='this is my first project'
+PACKAGES=['housing']
+REQUIREMENT_FILE_NAME='requirements.txt'
+
+def get_requirements_list()-> List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+)
